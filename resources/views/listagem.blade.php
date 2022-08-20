7f044c6876854c5d29b0785a5cfeee68c743cf9c
LucasRodrigues2409 <i-lucas@live.com>
2022-08-19 22:59:37 -0300

    Projeto Heroku Lição
    Projeto Heroku Lição

---
+++ resources/views/listagem.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-vidth, initial scale=1">
+        <title>LISTA DE CADASTRADOS</title>
+</head>
+<body>
+    <h1>Lista de Cadastrados</h1>
+    <p>Nome: Lucas Rodrigues Batista Idade: 16 email: i-lucas@live.com.br  </p>  
+    <table>    
+</table>
+</body>
+</html>
